dbad4882669641747bf0ef3bfbb97357d333265e
seema <simmid81@gmail.com>
2020-02-11 17:44:49 -0500

    walmartfirsttestcase

---
+++ Walmart/src/main/java/home/webelement/WebElementHomePage.java
@@ -0,0 +1,11 @@
+package home.webelement;
+
+public class WebElementHomePage {
+
+// All the Web Elements comes in this class from HomePage
+
+
+        public static final String webElementSearchBox="//*[@id=\"global-search-input\"]";
+        public static final String webElementSearchButton="//*[@id=\"global-search-submit\"]/span/span";
+
+    }
