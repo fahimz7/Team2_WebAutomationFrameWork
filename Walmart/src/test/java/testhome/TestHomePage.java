f656ca0ad7a4341377c309282cf4e5b76d78724e
seema <simmid81@gmail.com>
2020-02-12 16:59:21 -0500

    6 test case

--- Walmart/src/test/java/testhome/TestHomePage.java
+++ Walmart/src/test/java/testhome/TestHomePage.java
@@ -12,17 +12,49 @@ public class TestHomePage extends WebAPI {
 
 
     //PageFactory
-    public void getInItElements(){
-        homepage= PageFactory.initElements(driver,HomePage.class);
-        aboutpage= PageFactory.initElements(driver,AboutPage.class);
+    public void getInItElements() {
+        homepage = PageFactory.initElements(driver, HomePage.class);
+        aboutpage = PageFactory.initElements(driver, AboutPage.class);
 
 
     }
 
     @Test(priority = 1)
-    public void testDoSearch(){
+    public void testDoSearch() {
         getInItElements();
-        homepage.doSearch("mobile");
+        homepage.doSearch("boost mobile phones");
     }
 
+
+    @Test(priority = 2)
+    public void testDoSearchForTshirt() {
+        getInItElements();
+        homepage.doSearch("t-shirt");
+    }
+
+    @Test(priority = 3)
+    public void testDoSearchButton() {
+        getInItElements();
+        homepage.doSearchButton();
+    }
+
+    @Test(priority = 4)
+    public void testDoGroceryButton() {
+        getInItElements();
+        homepage.doSearchGrocery();
+
+    }
+
+    @Test(priority = 5)
+    public void testDoAccountButton() {
+        getInItElements();
+        homepage.doSearchAccount();
+
+    }
+
+    @Test(priority = 6)
+    public void testDoStoresLocationButton() {
+        getInItElements();
+        homepage.doSearchStoresLocation();
+    }
 }
